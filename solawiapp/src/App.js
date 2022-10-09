aa79952bd4123ac7aef0d8edb596b4aaf320b69b
Greg Carnon <krafttraining.coach@gmail.com>
2022-10-09 20:18:00 +0200

    Version 0.51

--- solawiapp/src/App.js
+++ solawiapp/src/App.js
@@ -1,25 +1,80 @@
 import Modal from "./Modal.js";
 import './App.css';
 import React, { useState } from "react";
+import Navbar from "./components/navigation.js";
+import EinzelnesFeld from "./components/einzelnesFeld.js";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="App">
+   
+   <div> 
+      <Navbar />
 
-        <div className="tunnelt1" onClick={() => setIsOpen(true)}>T1</div>
-        <div className="tunnelt2a">T2A</div>
-        <div className="tunnelt2b">T2B</div>
-        <div className="tunnelt3a">T3A</div>
-        <div className="tunnelt3b">T3B</div>
-        <div className="tunnelt4a">T4A</div>
-        <div className="tunnelt4b">T4B</div>
-        <div className="tunnelt5">T5</div>
+   <div class="container">
+    <div class="feld">
+        <div class="kleinesFeld feldTeiler feldHintergrund">
+            Feld 4
+        </div>
 
+        <div class="kleinesFeld feldTeilerGross feldHintergrund">
+            Feld 3
+        </div>
+
+        <div class="kleinesFeld feldTeiler feldHintergrund">
+            Feld 2
+        </div>
+
+        <div class="kleinesFeld feldTeiler feldHintergrund">
+            Feld 1
+        </div>
+
+    </div>
+
+
+
+    <div class="feld">
+        <div class="kleinesFeld feldTeiler feldHintergrund">
+            Feld 5
+        </div>
+
+        <div class="feldSechs feldHintergrund">
+            Feld 6
+        </div>
+
+        <div class="tunnel feldHintergrund">
+        Tunnel
+        </div>
+
+    </div>
+
+    <div class="feldSpezial">
+        
+    <div class="ghostFeld feldTeiler ">
+             
+        </div>
+
+        <div class="ghostFeld feldTeiler ">
+             
+        </div>
+        
+        <div class="kleinesFeld feldTeiler feldHintergrund">
+            Feld 7
+        </div>
+
+        <div class="kleinesFeld feldTeiler feldHintergrund">
+            Feld 8
+        </div>
+    </div>
+    
+    
+        
     <Modal open={isOpen} close={() => setIsOpen(false)}>
     <iframe width="500" height="400" src="https://www.solawineufarn.de/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Modal>
 
+        <div><EinzelnesFeld /></div>
+    </div>
 
     </div>
   );
